028324ee6c154675aa93ec58f794896709d907c2
tenaye123 <121737239+tenaye123@users.noreply.github.com>
2023-02-11 15:04:39 -0500

    this is the first change

---
+++ script.js
@@ -0,0 +1,11 @@
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.example.com/data", true);
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    console.log(xhr.responseText);
+  }
+};
+
+xhr.send();
