8fcabb7b35c00acb1ce4043c4977c14fc7fdc53b
tenaye123 <mulutenayee@gmail.com>
2023-02-13 21:39:17 -0500

    one of the following

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const xhr = new XMLHttpRequest();
+// const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://api.example.com/data", true);
+// xhr.open("GET", "https://api.example.com/data", true);
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    console.log(xhr.responseText);
-  }
-};
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+//     console.log(xhr.responseText);
+//   }
+// };
 
-xhr.send();
+// xhr.send();
+console.log("hello world");
